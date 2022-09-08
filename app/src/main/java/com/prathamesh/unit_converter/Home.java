9127609c592c0ac1d28bcb567036a1d1f2fab955
Prathamesh Adate <prathameshadate05@gmail.com>
2022-09-08 20:04:26 +0530

    Home Screen

---
+++ app/src/main/java/com/prathamesh/unit_converter/Home.java
@@ -0,0 +1,14 @@
+package com.prathamesh.unit_converter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
--- app/src/main/java/com/prathamesh/unit_converter/Splash.java
+++ app/src/main/java/com/prathamesh/unit_converter/Splash.java
@@ -2,13 +2,12 @@ package com.prathamesh.unit_converter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.ImageView;
 
 public class Splash extends AppCompatActivity {
-
-    ImageView splash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +17,9 @@ public class Splash extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-
+                Intent intent = new Intent(Splash.this, Home.class);
+                startActivity(intent);
+                finish();
             }
         },Constants.SPLASH_TIME);
     }
